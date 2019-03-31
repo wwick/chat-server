76c8ecbcc5f29b9b8b0f2a448307f8c647ee7050
wwick <wwick@wustl.edu>
2019-03-31 15:40:35 -0500

    showing list of current users works

--- project/index.js
+++ project/index.js
@@ -1,12 +1,14 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-let rooms = Array({
-  'name':'default',
-  'password':'default',
-  'users':Array(),
-  'owner':''
-});
+let rooms = {
+  'default':{
+    'users':Array(),
+    'owner':'',
+    'password':'',
+    'banned':''
+  } 
+};
 let users_list = {};
 const port = 3000;
 
@@ -17,50 +19,86 @@ app.get('/', function(req, res){
 io.on('connection', function(socket) {
 
   socket.on('chat message', function(msg) {
-    let room = users_list[msg.socket_id].room;
-    io.to(room).emit('chat message', msg);
+    let room_name = users_list[msg.socket_id].room;
+    let username = users_list[msg.socket_id].username;
+    let message = username + ": " + msg.message;
+    io.to(room_name).emit('message', message);
   });
 
   socket.on('login', function(credentials) {
     users_list[credentials.socket_id] = {
-      'username':credentials.username,
-      'room':'default'
+      'username':credentials.username
     };
+    joinRoom({
+      'name':'default',
+      'socket_id':credentials.socket_id
+    });
   });
 
-  socket.on('join room', function(room) {
-    let room_exists = false;
-    for (i = 0; i < rooms.length; i++) {
-      if (rooms[i].name == room.name){
-        if (rooms[i].password != room.password){
-          return false;
-        } else {
-          if (!rooms[i].users.includes(room.socket_id)) {
-            rooms[i].users.push(room.socket_id);
-          }
-          room_exists = true;
-        }
+  function joinRoom(room) {
+    let username = users_list[room.socket_id].username;
+    let message = username + " has joined the room";
+    io.to(room.name).emit('message', message);
+    let members = "(nobody)";
+    let room_users = rooms[room.name].users;
+    if (room_users.length != 0) {
+      members = users_list[room_users[0]].username;
+      for (i=1; i<room_users.length; i++) {
+        members += ", " + users_list[room_users[i]].username;
       }
     }
-    if (!room_exists) {
-      rooms.push({
-        'name':room.name,
-        'password':room.password,
-        'users':Array(room.socket_id),
-        'owner':room.socket_id
-      });
-    }
-    socket.leave(users_list[room.socket_id].room);
+    rooms[room.name].users.push(room.socket_id);
+    message = room.name + " currently contains: " + members;
+    io.to(room.socket_id).emit('join room', {
+      'success':true,
+      'message':message
+    });
     socket.join(room.name);
-    console.log(room.name);
-    console.log(rooms);
+    users_list[room.socket_id].room = room.name;
+  }
 
+  function leaveRoom(room) {
+    let former_room = users_list[room.socket_id].room;
+    socket.leave(former_room);
+    for (i=0; i<rooms[former_room].users.length; i++) {
+      if (rooms[former_room].users[i] == room.socket_id) {
+        rooms[former_room].users.splice(i,1);
+        continue;
+      }
+    }
+    let username = users_list[room.socket_id].username;
+    let message = username + ' has left the room';
+    io.to(former_room).emit('message', message);
+  }
+
+
+  socket.on('join room', function(room) {
+
+    if (rooms[room.name] == null) {
+      rooms[room.name] = {
+        'password':room.password,
+        'users':Array(),
+        'owner':room.socket_id,
+        'banned':''
+      };
+      leaveRoom(room);
+      joinRoom(room);
+      
+    } else {
+      if (rooms[room.name].password == room.password) {
+        if (!rooms[room.name].banned.includes[room.socket_id]) {
+          leaveRoom(room);
+          joinRoom(room);
+          return false;
+        }
+      }
+      io.to(room.socket_id).emit('join room', {
+        'success':false,
+        'message':'failed to join ' + room.name
+      });
+
+    }
   });
-
-  // socket.on('user join', function(username) {
-  //   console.log(username);
-  //   io.emit('user join', username);
-  // });
 
   socket.on('delete room', function(room){
     for (i = 0; i < rooms.length; i++) {
